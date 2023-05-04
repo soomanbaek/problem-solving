34f0fc9dfea909d904ba398ae97ec302ecf5c019
soomanbaek <72093196+soomanbaek@users.noreply.github.com>
2023-05-04 13:38:13 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.27 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+
+        int[] arr = new int[number+1];
+
+        for(int i=1; i<=number; ++i){
+            for(int j=1; j<=number/i; ++j) {
+                arr[i*j] += 1;
+            }
+            if(arr[i] > limit) answer += power;
+            else answer += arr[i];
+        }
+
+        return answer;
+    }
+}
