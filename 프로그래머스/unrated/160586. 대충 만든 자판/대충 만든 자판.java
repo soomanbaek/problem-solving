2d855ca19f1b09a070095b1849921a415870b633
soomanbaek <72093196+soomanbaek@users.noreply.github.com>
2023-05-04 11:35:14 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.48 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -4,15 +4,19 @@ class Solution {
     public static ArrayList solution(String[] keymap, String[] targets) {
         ArrayList answer = new ArrayList();
         HashMap<Character, Integer> hm = new HashMap<>();
-        for(int j=0; j<keymap.length; ++j){
-            for(int i=0; i<keymap[j].length(); ++i){
-                char c = keymap[j].charAt(i);
-                if(hm.containsKey(c) && hm.get(c) < i+1)  continue;
-                hm.put(c, i+1);
+        for(String key: keymap){
+            for(int i=0; i<key.length(); ++i){
+                char c = key.charAt(i);
+                if(hm.containsKey(c)){
+                    if(hm.get(c) > i+1)
+                        hm.replace(c, i+1);
+                }
+                else{
+                    hm.put(c, i+1);
+                }
             }
         }
-        for(int j=0; j< targets.length; ++j){
-            String target = targets[j];
+        for(String target: targets){
             int num = 0;
             for(int i=0; i<target.length(); ++i){
                 char c = target.charAt(i);
